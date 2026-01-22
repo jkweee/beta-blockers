9fb08e5a71d398d40b3c22a3021357259ba1d6ce
jkweee <j.jason.k@hotmail.com>
2026-01-22 12:58:23 +1100

    update append_sets with speed improvements from Gemini

--- append_sets.sh
+++ append_sets.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+# Ensures we are in the correct directory
 pwd
+
+# CPU/IO Priority: Only uses resources when the system is idle
 ionice -c3 nice -n 19 ffmpeg \
   -f concat \
   -safe 0 \
   -i input_files.txt \
   -c:v libx264 \
-  -preset slow \
+  -preset medium \
   -crf 18 \
   -pix_fmt yuv420p \
   -r 60 \
   -c:a aac \
   -b:a 256k \
-  -threads 2 \
-  -bufsize 32M \
-  -max_muxing_queue_size 512 \
-  2026-01-21-combined-highquality.mp4
+  -max_muxing_queue_size 1024 \
+  -maxrate 50M \
+  -bufsize 64M \
+  2026-01-22-combined-highquality.mp4
